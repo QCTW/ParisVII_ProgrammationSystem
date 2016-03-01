79b859da1f7789a6473dbcd42c481bbfa7ba85e1
HSIEH Yung-Kun <hsieh@informatique.univ-paris-diderot.fr>
2016-03-01 17:44:00 +0100

    TP6 Exercise 1

--- TP6_2.c
+++ TP6_2.c
@@ -23,15 +23,16 @@ struct Rendezvous
 
 int main (int argc, char* argv[])
 {
-	const char* semaphoreName = "SemaphoreSHM3";
+	const char* semaphoreName = "SemaphoreSHM";
 	struct Rendezvous* rdv; //Pointer points to the memory created by mmap to be used for Rendezvous
-	int memId = shm_open(semaphoreName, O_CREAT|O_RDWR, 0666);
-	if( memId<=0 )
+	int memId = shm_open(semaphoreName, O_CREAT|O_RDWR, 0644);
+	if( memId<0 )
 	{
-	    perror("[1]shm_open() error");
+		printf("%d\n",memId);
+	    perror("[2]shm_open() error");
 	    exit(-1);
 	}
-	printf("[1]shm_open() created shared memory with id %d\n", memId);
+	printf("[2]shm_open() created shared memory with id %d\n", memId);
 
 	//Problem on MAC with 2nd execution of ftruncate
 	//Check http://stackoverflow.com/questions/25502229/ftruncate-not-working-on-posix-shared-memory-in-mac-os-x
@@ -40,35 +41,35 @@ int main (int argc, char* argv[])
 	{
 		if ( ftruncate(memId, sizeof(rdv)) < 0 )
 		{
-			perror("[1]ftruncate() error");
+			perror("[2]ftruncate() error");
 			exit(-1);
 		}
 	}
-	printf("[1]ftruncate() shared memory to %lu\n", sizeof(rdv));
+	printf("[2]ftruncate() shared memory to %lu\n", sizeof(rdv));
 
 	rdv = mmap(NULL, sizeof(rdv), PROT_READ|PROT_WRITE, MAP_SHARED, memId, 0);
 	if(rdv==MAP_FAILED)
 	{
-		perror("[1]Map failed");
+		perror("[2]Map failed");
 		exit(-1);
 	}
 
-	printf("[1]mmap() created successful\n");
+	printf("[2]mmap() created successful\n");
 	rdv->value = 0;
 	sem_init(&rdv->vide, 1, 1); //This does not work on MAC
-	printf("[1]sem_init(&rdv->vide, 1, 1) executed\n");
+	printf("[2]sem_init(&rdv->vide, 1, 1) executed\n");
 	sem_init(&rdv->plein, 1, 0);
-	printf("[1]sem_init(&rdv->plein, 1, 0) executed\n");
+	printf("[2]sem_init(&rdv->plein, 1, 0) executed\n");
 	//rdv->vide = *sem_open("Rdv_Vide", O_CREAT|O_EXCL, S_IWGRP|S_IWOTH, 1);
 	//rdv->plein = *sem_open("Rdv_Plein", O_CREAT|O_EXCL, S_IWGRP|S_IWOTH, 0);
 	while(1)
 	{
-		sem_wait(&rdv->vide); //If not zero, block
-		printf("[1]Rendezvous.value after sem_wait()=%d\n", rdv->value);
+		sem_wait(&rdv->plein);
+		printf("[2]Rendezvous.value after sem_wait()=%d\n", rdv->value);
 		rdv->value = rand()%100;
-		printf("[1]Changed value=%d\n", rdv->value);
-		sem_post(&rdv->plein);
-		printf("[1]Rendezvous.value after sem_post()=%d\n", rdv->value);
+		printf("[2]Changed value=%d\n", rdv->value);
+		sem_post(&rdv->vide);
+		printf("[2]Rendezvous.value after sem_post()=%d\n", rdv->value);
 		sleep(1);
 	}
 }
