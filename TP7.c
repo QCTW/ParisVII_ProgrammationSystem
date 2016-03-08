5d3d9c53774d819f1b7f3b0d5724523e1c0f5293
HSIEH Yung-Kun <hsieh@informatique.univ-paris-diderot.fr>
2016-03-08 15:06:36 +0100

    Change signal() to sigaction()

--- TP7.c
+++ TP7.c
@@ -22,17 +22,24 @@ int main (int argc, char* argv[])
 	int rc = fork();
 	if(rc==0) //Child process
 	{
-		signal(SIGUSR1, signal_callback_handler);
-		while(1)
-		{
-			printf("La belle: Humm...\n");
-			sleep(3);
-		}
+		//signal(SIGUSR1, signal_callback_handler);
+		struct sigaction act;
+		act.sa_handler = signal_callback_handler;
+		act.sa_flags = 0;
+		sigaction(SIGUSR1, &act, NULL); //const struct sigaction *restrict act, struct sigaction *restrict oact);
+		printf("La belle: Humm...zzZz\n");
+		pause();
 	}
 	else
 	{
-		printf("Le price: Ma belle #%d j'arrive!\n", rc);
-		sleep(10);
+		printf("Le price: Ma belle #%d j'arrive pour vous!\n", rc);
+		int count = 5;
+		while(count>0)
+		{
+			printf("Le price: Ma belle #%d j'arrive dans %d secs!\n", rc, count);
+			sleep(1);
+			count--;
+		}
 		kill(rc, SIGUSR1);
 	}
 	return 0;
